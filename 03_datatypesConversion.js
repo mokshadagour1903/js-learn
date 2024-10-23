fb780710f806c80d5431a62d8edb6e5b93753238
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-10-23 10:19:58 +0000

    datatypes-conversion

---
+++ 03_datatypesConversion.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" ==> 33 
+// "33abc" ==> NaN
+// true => 1 , false => 0
+
+
+let isloggedIn = ""
+//console.log(isloggedIn)
+let booleanisLoggedIn  = Boolean(isloggedIn)
+//console.log(booleanisLoggedIn)
+
+//1  => true
+//"" => false
+//"moka" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
