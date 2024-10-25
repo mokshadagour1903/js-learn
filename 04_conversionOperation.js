6dbace697e9479fc7109c3898e11af558adc8550
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-10-25 09:58:30 +0000

    ConversionOperation - part1

---
+++ 04_conversionOperation.js
@@ -0,0 +1,36 @@
+/* ***********OPERATIONS************8 */
+
+let value = 3
+let negValue = -value
+// console.log(value)
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/3)
+// console.log(2**2)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1 ="hello"
+let str2=" moka"
+
+let str3 = str1+str2
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 4 + "2")
+//console.log(+true)
+
+// 04_comparision.js
+
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+
+// console.log(2!=1)
+
