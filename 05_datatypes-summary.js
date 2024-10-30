5b03190ce21954e50567053d0a7515c9b1ffb7ef
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-10-30 15:58:01 +0000

    Operation Coversions And Datatypes- Summary

---
+++ 05_datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive datatypes
+
+// 7 Types -- String , Boolean , Number , undefined(value not assigned) , Symbol(to make any value unique) , null (ka mtlb h empty), BigInt
+
+// Reference (Non-Primitive)
+
+// Arrays , Objects , Functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+//In typescript we do like this
+//const score2 :Number = 10
+const bigNumber = 657468767657686573678652n  //as soon as u add n to the last of the number it gets converted to big int number
+
+const heroes = ["shaktiman" , "naagraj" , "doga"]
+//designation : "soon to be a proficient backend Developer",
+let myObj = {
+    name : "Moka" ,
+    age : 24
+}
+const myFunction = function(){
+    console.log("Namstey everyone")
+}
+
+console.log(typeof bigNumber)
+console.log(typeof myObj)
+console.log(typeof heroes)
+console.log(typeof userEmail)
+console.log(typeof outsideTemp)
+console.log(typeof myFunction)
