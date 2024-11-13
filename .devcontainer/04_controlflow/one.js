b5ea55c5b52dc475980bc300aad75c037a5808ab
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-11-13 07:09:59 +0000

    ControlFlow Statements in Javascript

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,51 @@
+// if 
+
+// const isUserLoggedIn = true
+if(2 == "2"){
+    // console.log("executed")
+    }
+    
+    // <, >,<=,>=,==,!=,=== , !==
+
+    // const score = 200
+
+    // if(score >100)
+    // {
+    //   let power = "fly"
+    //     console.log(`User Power:${power}`)
+    // }
+    // console.log(`User Power:${power}`)
+
+    //SHORTHAND NOTATION
+
+//     const balance =1000
+//     // if(balance>500) console.log("test"),console.log("test2");
+
+//     if(balance<500)
+//     {
+//        console.log("Less than ")
+//     }
+//     else if(balance < 750)
+//     {
+//         console.log("Less than 750")  
+//     }
+//     else if(balance < 900){
+//         console.log("Less than 900") 
+//     }
+//   else{
+//     console.log("Less than 1200") 
+//   }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle =false 
+const loggedInFromEmail = true 
+if(userLoggedIn && debitCard && 2==3)
+{
+    console.log("Allow to buy course")
+}
+
+if (loggedInFromGoogle || loggedInFromEmail)
+{
+   console.log("User Logged In");
+}
---
+++ .devcontainer/04_controlflow/truthy.js
@@ -0,0 +1,50 @@
+const userEmail = "m@gmail.com"
+if(userEmail)
+{
+    console.log("got the user email")
+
+}
+else {
+    console.log("Don't have the user email")
+}
+
+// FALSY VALUES 
+
+//false, 0, -0, BigInt 0n , "", null, undefined, NaN
+
+//TRUTHY VALUES 
+
+// "0" , 'false' , " ", [], {}, function(){}
+
+if(userEmail.length === 0){
+    console.log("Array is empty")
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty")
+}
+
+// In console 
+//false == 0 , 0 == false , 0 == ''  --> it gives true 
+
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 =5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 100 ?? 19
+// console.log(val1)
+
+//TERNIARY OPERATOR 
+
+
+//  condition  ? true : false 
+
+const iceTeaPrice =100
+
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
