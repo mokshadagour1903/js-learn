6bcf608f4ab5cb02ded56c023c806b9220537a9a
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-11-13 19:51:13 +0000

    Iterations in javascript

---
+++ .devcontainer/05_iterations/eight.js
@@ -0,0 +1,39 @@
+const myNums = [1,2,3]
+// const myTotal = myNums.reduce(function(acc, currval){
+//     console.log(`acc : ${acc}, currval ${currval}`);
+    
+//     return acc + currval ;
+// },0)
+
+// const myTotal = myNums.reduce((acc, currval)=> acc+ currval,0)
+// console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName :"js course",
+        price:2999
+    },
+    {
+        itemName :"py course",
+        price:3999
+    },
+    {
+        itemName :"cpp course",
+        price:999
+    },
+    {
+        itemName :"java course",
+        price:4999
+    },
+    {
+        itemName :"mobile development course",
+        price:9999
+    },
+    {
+        itemName :"Data Science course",
+        price:12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc,item) => acc + item.price, 0)
+console.log(priceToPay);
---
+++ .devcontainer/05_iterations/four.js
@@ -0,0 +1,25 @@
+const myObj ={
+    js :'javascript',
+    cpp:'C++',
+    rb:"Ruby",
+    swift:"Swift by apple"
+}
+
+// for (const key in myObj) {
+//    console.log(`${key} shortcut is for ${myObj[key]}`)
+// }
+
+// const programming = ["js","rb","python","java","dotnet","cpp"]
+// for (const key in programming) {
+//  console.log(programming[key]);
+// }
+
+// const map = new Map() --> it will not work (for in loop will not work on map)
+// map.set('IN', "India")
+// map.set('USA', "United States Of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+   
+//        console.log(key) 
+//     }
