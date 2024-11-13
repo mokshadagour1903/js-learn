d5f3e5d097d4569dad8ec035ff5adf4d04bea5d6
mokshadagour1903 <127046984+mokshadagour1903@users.noreply.github.com>
2024-11-13 09:51:54 +0000

    Whilw and do while loop

---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0 ;
+// while (index <= 10) {
+//    console.log(`Value of index is ${index}`) 
+//    index = index+2;
+// }
+
+// let myArray = [10,20,30,40,50,60,70,80,90,100]
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`Value of myArray is ${myArray[arr]}`)
+//     arr = arr+1
+// }
+
+//do while 
+
+
+// let score =11
+//  do {
+//     console.log(`Score is ${score} `)
+//     score++
+//  }while(score <=10)
